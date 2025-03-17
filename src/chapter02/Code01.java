85071d3558029c79b211bdb80ea7f349001fcf49
jeaghojun <hazartckry@gmail.com>
2025-03-17 11:52:29 +0900

    main method 사용법, 연결연산자, 덧셈연산자, println과 print 차이점

---
+++ src/chapter02/Code01.java
@@ -0,0 +1,12 @@
+package chapter02;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Code01 {
+    public static void main(String[] args){
+        System.out.println("AIsoftware");
+        System.out.println("hojun jeag");
+        System.out.printf("Grade 1\n");
+        System.out.println("result:"+(100+200));
+    }
+}
